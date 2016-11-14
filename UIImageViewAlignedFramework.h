14d6d37384dee03d62a1c68d4ed0ec3f0ad26605
Paweł Kania <pwlkania@gmail.com>
2016-11-14 13:13:36 +0100

    Added Carthage support

---
+++ UIImageViewAlignedFramework.h
@@ -0,0 +1,19 @@
+//
+//  UIImageViewAlignedFramework.h
+//  UIImageViewAlignedFramework
+//
+//  Created by Paweł Kania on 14/11/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageViewAlignedFramework.
+FOUNDATION_EXPORT double UIImageViewAlignedFramework_VersionNumber;
+
+//! Project version string for UIImageViewAlignedFramework.
+FOUNDATION_EXPORT const unsigned char UIImageViewAlignedFramework_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageViewAlignedFramework/PublicHeader.h>
+
+
